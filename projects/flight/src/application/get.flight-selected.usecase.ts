25bfbf977e8f8a7b736f7ad6d0bfdd3a3bbc8c69
KgalarzaSofka <kevin.galarza@sofka.com.co>
2025-02-09 10:14:11 -0500

    feat se agrega caso de uso para obtener el etado de flidhts selcted

---
+++ projects/flight/src/application/get.flight-selected.usecase.ts
@@ -0,0 +1,30 @@
+import { inject, Injectable } from '@angular/core';
+import { GetFlightsService } from '../infrastructure/services/get-flight.service';
+import { State } from '../domain/state';
+import { finalize, Observable, Subscription, take, tap } from 'rxjs';
+import { IFlight } from '../domain/model/flight';
+import { LoadingService } from 'shared';
+import { FlightSearchCriteria } from '../infrastructure/ui/interfaces/flight-search-criteria';
+import { Router } from '@angular/router';
+import { IFlightSelected } from '../domain/model/flight-selected';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GetFlightsUseCase {
+  private readonly _state = inject(State);
+  private subscriptions!: Subscription;
+
+  flight$(): Observable<IFlightSelected> {
+    return this._state.flightsSelected.$();
+  }
+
+  initSubscription(): void {
+    this.subscriptions = new Subscription();
+  }
+
+  destroySubscription(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+}
